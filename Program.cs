e7ce86a13304d1e26cc2d962d7a2ea1551243df8
Firingo <petr.havlin.0605@gmail.com>
2022-07-28 12:29:19 +0200

    4-5 Lekce - Středně pokročilá úloha

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Zadejte delší slovo");
+string slovoDlouhe = Console.ReadLine();
+Console.WriteLine("Zadejte kratší slovo");
+string slovoKratke = Console.ReadLine();
+
+int dlouhe = slovoDlouhe.Length;
+int kratke = slovoKratke.Length;
+int rozdil = dlouhe - kratke;
+Console.WriteLine("Slova se liší délkou o " + rozdil + " znaků.");
+Console.ReadLine();
